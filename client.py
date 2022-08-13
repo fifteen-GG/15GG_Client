537bcde7f47c10c627f8c8234b3bc5b8587bb0aa
James <jamesj0107@naver.com>
2022-08-13 20:34:57 +0900

    feat: init replay client

---
+++ client.py
@@ -0,0 +1,40 @@
+import subprocess
+
+game_dir = 'C:/Riot Games/League of Legends'
+replay_dir = 'C:/Users/James/Documents/League of Legends/Replays'
+
+
+class ReplayClient():
+    """
+    League of Legend replay Client class
+
+    Opens a League of Legends client
+
+    """
+
+    def __init__(self, game_dir, replay_dir):
+        self.game_dir = game_dir
+        self.replay_dir = replay_dir
+
+    def run_client(self):
+        subprocess.Popen(
+            [
+                self.game_dir + '/Game/League of Legends.exe'
+                # TODO dynamically get replay file name
+                self.replay_dir + 'KR-6072648122.rofl',
+                '-GameBaseDir=' + self.game_dir,
+                '-Region=KR',  # TODO set region dynamically
+                '-PlatformID=KR',
+                '-Locale=ko_KR',
+                '-SkipBuild',
+                '-EnableCrashpad=true',
+                '-EnableLNP',
+                '-UseDX11=1:1',
+                '-UseMetal=0:1',
+                '-UseNewX3D',
+                '-UseNewX3DFramebuffers',
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            cwd=game_dir
+        )
