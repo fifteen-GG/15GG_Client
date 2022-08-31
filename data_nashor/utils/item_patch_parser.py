e385f42fef30d80802c7655def1340fe8ab5f824
Jinhyung Lee <luciancah@gmail.com>
2022-08-31 23:31:29 +0900

    feat: parse latest patchc version from datadragon

--- data_nashor/utils/item_patch_parser.py
+++ data_nashor/utils/item_patch_parser.py
@@ -1,9 +1,33 @@
+from errno import EL
 import json
+from logging import exception
+from tkinter import E
 import requests
+from bs4 import BeautifulSoup
 
 
-def item_patch_parser(patch_num, dest_dir):
-    patch = patch_num
+def patch_version_parser():
+    url = 'https://developer.riotgames.com/docs/lol#data-dragon_data-assets'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        version = soup.select_one(
+            'body > div.container > div.layout > div.content-container > div.content > p:nth-child(69) > code:nth-child(1)')
+    else:
+        raise Exception('서버 접속 불가')
+
+    return version.get_text()
+
+
+def item_patch_parser(dest_dir):
+    try:
+        patch_num = patch_version_parser()
+    except:
+        print('버전 파싱 에러')
+        return
+
     url = requests.get(
         f'https://ddragon.leagueoflegends.com/cdn/{patch_num}/data/ko_KR/item.json')
     text = url.text
