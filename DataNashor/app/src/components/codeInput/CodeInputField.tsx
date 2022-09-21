72a39fa3f0624cf7467cd5c7096752b396e89f26
tjdcjf0222 <tjd000222@naver.com>
2022-09-21 14:39:07 +0900

    feat: Add auto-focus to next input

--- DataNashor/app/src/components/codeInput/CodeInputField.tsx
+++ DataNashor/app/src/components/codeInput/CodeInputField.tsx
@@ -1,8 +1,10 @@
-import { ChangeEvent, useState } from "react";
-import { CodeInputFieldWrapper, InputField } from "./styles/codeInputField.s";
+import { buildTimeValue } from '@testing-library/user-event/dist/utils';
+import { useRef } from 'react';
+import { ChangeEvent, useState } from 'react';
+import { CodeInputFieldWrapper, InputField } from './styles/codeInputField.s';
 
 export const CodeInputField = () => {
-  const [code, setCode] = useState<string[]>(["", "", "", "", "", ""]);
+  const [code, setCode] = useState<string[]>(['', '', '', '', '', '']);
 
   const handleInput = (e: ChangeEvent<HTMLInputElement>, index: number) => {
     let updatedCode = [...code];
@@ -10,16 +12,23 @@ export const CodeInputField = () => {
     setCode(updatedCode);
   };
 
+  const ref = useRef<HTMLInputElement[]>([]);
+
   return (
     <CodeInputFieldWrapper>
       {code.map((_, i) => {
         return (
           <InputField
+            type={'text'}
+            ref={(elem) => (ref.current[i] = elem!)}
             maxLength={1}
             key={i}
             value={code[i]}
             onChange={(e) => {
               handleInput(e, i);
+              if (e.target.value.length === 1) {
+                ref.current[i + 1]!.focus();
+              }
             }}
           />
         );
