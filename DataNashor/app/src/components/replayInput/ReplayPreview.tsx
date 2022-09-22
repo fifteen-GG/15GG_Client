cec0fde4ed1a106b5dabc17c17b82a418e36422b
James Jung <therealjamesjung@gmail.com>
2022-09-22 15:15:47 +0900

    style: Fix minor styles

--- DataNashor/app/src/components/replayInput/ReplayPreview.tsx
+++ DataNashor/app/src/components/replayInput/ReplayPreview.tsx
@@ -1,5 +1,5 @@
 import { ReplayPreviewWrapper } from "./styles/replayPreview.s";
 
 export const ReplayPreview = () => {
-  return <ReplayPreviewWrapper>2022-09-11 / 패치 12.17</ReplayPreviewWrapper>;
+  return <ReplayPreviewWrapper>2022-09-11 · 패치 12.17</ReplayPreviewWrapper>;
 };
