91ff3b4389097c6ccbc1bbb424644b5207e1d9de
SungChul Hong <tjd000222@naver.com>
2022-09-30 16:24:52 +0900

    feat: Add basic function of dragndrop using dropzone library

--- DataNashor/app/src/components/replayInput/ReplayFileName.tsx
+++ DataNashor/app/src/components/replayInput/ReplayFileName.tsx
@@ -1,9 +1,21 @@
 import { ReplayFileNameWrapper } from './styles/replayFileName.s';
+import React, { useCallback } from 'react';
+import { useDropzone, FileWithPath } from 'react-dropzone';
 
 export const ReplayFileName = () => {
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+  const files = acceptedFiles.map((file: FileWithPath) => (
+    <li key={file.path}>{file.name}</li>
+  ));
+
   return (
-    <ReplayFileNameWrapper>
-      <u>KR1-281823819.rofl</u>
+    <ReplayFileNameWrapper
+      onDrop={(acceptedFiles) => console.log(acceptedFiles)}
+    >
+      <div {...getRootProps({ className: 'dropzone' })}>
+        <input {...getInputProps()} />
+        <u>파일을 입력해 주세요.{files}</u>
+      </div>
     </ReplayFileNameWrapper>
   );
 };
