fbb56d5b33f9264ea46f870f5ae724f338587725
Jinhyung Lee <luciancah@gmail.com>
2022-11-10 02:41:05 +0900

    refactor: [OD15-76] remove overlay directory

--- DataNashor/overlay/src/components/winRate/LeagueTeams.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import * as Palette from '../../assets/colorPalette';
-
-import { TeamsWrapper } from './styles/leagueTeams.s';
-
-export const LeagueTeams = () => {
-  return <TeamsWrapper></TeamsWrapper>;
-};
